99b6fd3b5dc60eefedde301b0a16ad103b57c16a
Brandon Roberts <brandon.roberts@synapse-wireless.com>
2018-02-16 15:04:23 -0600

    Example app

--- src/test.ts
+++ src/test.ts
@@ -9,15 +9,18 @@ import 'zone.js/dist/fake-async-test';
 import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
-  platformBrowserDynamicTesting
+  platformBrowserDynamicTesting,
 } from '@angular/platform-browser-dynamic/testing';
 
+// fixes typing errors in Atom editor
+import {} from 'jasmine';
+
 // Unfortunately there's no typing for the `__karma__` variable. Just declare it as any.
-declare const __karma__: any;
-declare const require: any;
+declare var __karma__: any;
+declare var require: any;
 
 // Prevent Karma from running prematurely.
-__karma__.loaded = function () {};
+__karma__.loaded = function() {};
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(
